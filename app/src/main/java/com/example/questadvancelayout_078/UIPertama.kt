7dc04163c7a6fd397edf0cb17e65ec95a4c15824
dimas <dimaswh19@gmail.com>
2025-10-23 12:36:23 +0700

    Menambahkan teks judul program studi dengan ukuran dan ketebalan font

--- app/src/main/java/com/example/questadvancelayout_078/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_078/UIPertama.kt
@@ -31,3 +31,6 @@ fun ActivitasPertama(modifier: Modifier){
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = stringResource(id=R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
